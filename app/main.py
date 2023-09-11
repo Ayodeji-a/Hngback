90538d74ba72feea2a88572fe6829629356d4f79
Tolu Soneye <68499075+sammy-jay@users.noreply.github.com>
2023-09-11 22:12:16 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def index(slack_name: str = "yuba", track: str = "backend"):
         "current_day": current_day,
         "utc_time": formatted_utc_time,
         "track": track,
-        "github_file_url": "https://github.com/Ayodeji-a/Hngback/main/app/main.py",
+        "github_file_url": "https://github.com/Ayodeji-a/Hngback/blob/main/app/main.py",
         "github_repo_url": "https://github.com/Ayodeji-a/Hngback.git",
         "status_code": 200,
     }
